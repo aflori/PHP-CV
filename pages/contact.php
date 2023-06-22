b154fc6524c10b616c69238c83ead2e14254bef9
aflori <aurelien.flori@le-campus-numerique.fr>
2023-06-22 10:51:14 +0200

    bugfixe du formulaire (étape 4.1)

--- pages/contact.php
+++ pages/contact.php
@@ -5,11 +5,11 @@ include 'header.php'
 ?>
 
 <main>
-    <form action="http://phpdebase.local/?page=contact" method="post">
+    <form action="http://phpdebase.local/?page=contact/" method="post">
         <div class="m-5">
             <div>
                 <label class="input-group-text" for="civilite">civilité</label>
-                <select class="form-select" id="civilite">
+                <select class="form-select" name="civilite">
                 <option selected>Quelle est votre civilité</option>
                 <option value="M.">M.</option>
                 <option value="Mme">Mme</option>
@@ -17,32 +17,32 @@ include 'header.php'
             </div>
             <div class="mb-3">
                 <label for="name1" class="form-label">Votre nom</label>
-                <input type="text" class="form-control" id="name1" placeholder="Votre prénom">
+                <input type="text" class="form-control" name="name1" placeholder="Votre prénom">
             </div>
             <div class="mb-3">
                 <label for="name2" class="form-label">Votre prénom</label>
-                <input type="text" class="form-control" id="name2" placeholder="Votre NOM">
+                <input type="text" class="form-control" name="name2" placeholder="Votre NOM">
             </div>
             <div class="mb-3">
                 <label for="Adresse email" class="form-label">Votre adresse email</label>
-                <input type="email" class="form-control" id="Adesse email" placeholder="name@example.com">
+                <input type="email" class="form-control" name="Adesse email" placeholder="name@example.com">
             </div>
             <div class="mb-3">
                 <label for="emploi" class="form-label">Le motif de la demande:</label>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="motif demande" id="emploi">
+                    <input class="form-check-input" type="radio" name="motif demande" value="emploi" >
                     <label class="form-check-label" for="emploi">
                         Demande d'emploi
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="motif demande" id="informations">
+                    <input class="form-check-input" type="radio" name="motif demande" value="informations">
                     <label class="form-check-label" for="informations">
                         Demande d'information
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="motif demande" id="prestations" checked>
+                    <input class="form-check-input" type="radio" name="motif demande" value="prestations" checked>
                     <label class="form-check-label" for="prestations">
                         prestations
                     </label>
@@ -50,7 +50,7 @@ include 'header.php'
             </div>
             <div class="mb-3">
                 <label for="message complet" class="form-label">Message</label>
-                <textarea class="form-control" id="message complet" rows="3"></textarea>
+                <textarea class="form-control" rows="3" placeholder="Message" name="Message"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Envoyer</button>
 
